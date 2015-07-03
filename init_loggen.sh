089804e50c86b7f4a1e76f714e32643438eddd4e
rpwr021 <rpawar.021@gmail.com>
2015-07-03 20:55:20 +0530

    scriptupdated

--- init_loggen.sh
+++ init_loggen.sh
@@ -12,7 +12,17 @@ dkrurl=`sudo docker inspect -f  '{{json .}}' 4094074d | jshon  -e NetworkSetting
 if nc -zv $dkrurl 9000 ; then
 	java logGen $1
 else 
-	echo "docker container isn't up ? "
+	echo "docker container isn't up ?,
+	trying to start"
+	 
+	docker run -p 9000:9000 -it sequenceiq/hadoop-docker:2.6.0.V1 /etc/bootstrap.sh -bash
+
 fi
 
 #docker run   -e FLUME_AGENT_NAME=agentx  -e FLUME_CONF_FILE=${HOME}/NonClut/gitwrk/Flume_pipe/Simple_flumeLogr.conf
+
+
+#${HOME}/NonClut/flume/bin/flume-ng agent -c conf -f ~/NonClut/gitwrk/Flume_pipe/loglistener.conf -n loglistener
+#${HOME}/NonClut/flume/bin/flume-ng agent -c conf -f ~/NonClut/gitwrk/Flume_pipe/logwriter.conf -n logwriter
+
+
